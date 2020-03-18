2bc852a34fd10ef5089317f402c7b9332c029c46
Failpwned <alexander.j.shen@gmail.com>
2020-03-18 16:12:39 -0700

    Add functionality for touch input

--- MainCamera.cs
+++ MainCamera.cs
@@ -4,110 +4,188 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    public static MainCamera Current { get; set; }
+
+    public static class INPUT
+    {
+        public const int DRAG_THRESHOLD = 400; // squared
+        public const float MAX_LOOK_DISTANCE = 20f;
+        public const float MIN_LOOK_DISTANCE = 6f;
+        public const float ZOOM_SPEED = 0.001f;
+        public const float ROTATION_SPEED = 0.001f;
+    }
+
     private bool isDragging;
-    Vector2 touchStart;
-    Camera mainCamera;
+    private Vector2 touchStart;
+    private float initialTouchDistance;
+    private Camera mainCamera;
 
     // Mouse only
     Vector3 mousePosition;
     Vector3 lastMousePosition;
 
-    bool isCameraLocked = true;
+    private bool isCameraLocked = true;
+    private float lookDistance;
+    private bool secondTouchInitiated = false;
+
+    private Vector3 lookTarget;
+    public Vector3 LookTarget
+    {
+        get
+        {
+            return lookTarget;
+        }
+        set
+        {
+            if(lookTarget != value)
+            {
+                lookTarget = value;
+                if (!isCameraLocked)
+                {
+                    isCameraLocked = true;
+                    StartCoroutine(SwitchLookTargets(lookTarget));
+                }
+            }
+            print(lookTarget);
+        }
+    }
+
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+        LookTarget = Vector3.zero;
+        Current = this;
+    }
 
     void Start()
     {
-        mainCamera = GetComponent<Camera>();
         StartCoroutine(LevelIntro());
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (Input.touchCount > 0)
+        if (false)
         {
-            Touch touch = Input.GetTouch(0);
-            print("WE HAS A TOUCH");
-            
-            switch (touch.phase)
+            if (!isCameraLocked)
             {
-                case TouchPhase.Began:
-                    touchStart = touch.position;
-                    break;
+                if (Input.touchCount == 1)
+                {
+                    Touch touch = Input.GetTouch(0);
 
-                case TouchPhase.Moved:
-                    isDragging = true;
-                    break;
+                    if (secondTouchInitiated)
+                    {
+                        touchStart = touch.position;
+                        secondTouchInitiated = false;
+                    }
 
-                case TouchPhase.Ended:
+                    switch (touch.phase)
+                    {
+                        case TouchPhase.Began:
+                            touchStart = touch.position;
+                            break;
+
+                        case TouchPhase.Moved:
+                            if((touch.position - touchStart).sqrMagnitude > INPUT.DRAG_THRESHOLD)
+                            {
+                                isDragging = true;
+                            }
+                            if (isDragging)
+                            {
+                                transform.Translate((touchStart - touch.position) * lookDistance * INPUT.ROTATION_SPEED, Space.Self);
+                                touchStart = touch.position; 
+                            }
+                            break;
+
+                        case TouchPhase.Ended:
+                            if (isDragging)
+                            {
+
+                            }
+                            else
+                            {
+                                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                                if (Physics.Raycast(ray, out RaycastHit hit))
+                                {
+                                    Primitive3D hitPrimitive = hit.collider.gameObject.GetComponent<CollisionHandler>().ParentPrimitive;
+                                    LevelManager.Current.SelectFace(hitPrimitive.GetFaceByNormal(hit.normal));
+                                }
+                            }
+                            isDragging = false;
+                            break;
+                    }
+
+                    
+                }
+                else if (Input.touchCount == 2)
+                {
+                    float touchDistance = (Input.GetTouch(1).position - Input.GetTouch(0).position).magnitude;
+                    if (!secondTouchInitiated)
+                    {
+                        initialTouchDistance = touchDistance;
+                        isDragging = true;
+                        secondTouchInitiated = true;
+                    }
+                    lookDistance = Mathf.Clamp(lookDistance + ((touchDistance - initialTouchDistance) * INPUT.ZOOM_SPEED), INPUT.MIN_LOOK_DISTANCE, INPUT.MAX_LOOK_DISTANCE);
+                }
+                if (!isCameraLocked)
+                {
+                    transform.LookAt(LookTarget);
+                    transform.position = transform.position.normalized * lookDistance; 
+                }
+            }
+        }
+        else
+        {
+            if (!isCameraLocked)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    mousePosition = Input.mousePosition;
+                    lastMousePosition = Input.mousePosition;
+                }
+
+
+
+                if (Input.GetMouseButton(0))
+                {
                     if (isDragging)
                     {
-
+                        transform.Translate((Input.mousePosition - lastMousePosition).normalized * 0.1f, Space.Self);
+                        lastMousePosition = Input.mousePosition;
                     }
                     else
                     {
-                        Ray ray = mainCamera.ScreenPointToRay(touch.position);
-                        RaycastHit hit;
-                        if(Physics.Raycast(ray, out hit))
+                        if (Vector3.Distance(Input.mousePosition, mousePosition) > 10)
                         {
-                            print("AWEFW");
+                            isDragging = true;
                         }
                     }
-                    isDragging = false;
-                    break;
-            }
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            print("AWEFWE");
-        }
-        */
-        if (!isCameraLocked)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                mousePosition = Input.mousePosition;
-                lastMousePosition = Input.mousePosition;
-            }
-
-
-
-            if (Input.GetMouseButton(0))
-            {
-                if (isDragging)
-                {
-                    transform.Translate((Input.mousePosition - lastMousePosition).normalized * 0.1f, Space.Self);
-                    lastMousePosition = Input.mousePosition;
                 }
-                else
+
+                if (Input.GetMouseButtonUp(0) && Vector3.Distance(Input.mousePosition, mousePosition) < 10)
                 {
-                    if (Vector3.Distance(Input.mousePosition, mousePosition) > 10)
+                    if (isDragging)
                     {
-                        isDragging = true;
+                        isDragging = false;
+                    }
+                    else
+                    {
+                        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                        if (Physics.Raycast(ray, out RaycastHit hit))
+                        {
+                            Primitive3D hitPrimitive = hit.collider.gameObject.GetComponent<CollisionHandler>().ParentPrimitive;
+                            LevelManager.Current.SelectFace(hitPrimitive.GetFaceByNormal(hit.normal));
+                        }
                     }
                 }
-            }
 
-            if (Input.GetMouseButtonUp(0) && Vector3.Distance(Input.mousePosition, mousePosition) < 10)
-            {
-                if (isDragging)
+                if (!isCameraLocked)
                 {
-                    isDragging = false;
-                }
-                else
-                {
-                    Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-                    if (Physics.Raycast(ray, out RaycastHit hit))
-                    {
-                        Primitive3D hitPrimitive = hit.collider.gameObject.GetComponent<CollisionHandler>().ParentPrimitive;
-                        LevelManager.Current.SelectFace(hitPrimitive.GetFaceByNormal(hit.normal));
-                    }
+                    transform.LookAt(LookTarget);
+                    transform.position = transform.position.normalized * 12;
                 }
             }
-
-
-            transform.LookAt(Vector3.zero);
-            transform.position = transform.position.normalized * 12;
         }
     }
 
@@ -121,6 +199,21 @@ public class MainCamera : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, COMMON.MATHFUNCTIONS.SquaredSmooth(startY, endY, time), transform.position.z);
             time += 0.01f;
+            yield return null;
+        }
+        isCameraLocked = false;
+        lookDistance = transform.position.magnitude;
+    }
+
+    private IEnumerator SwitchLookTargets(Vector3 newLookTarget)
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.LookRotation(newLookTarget - transform.position, Vector3.up);
+        float time = 0f;
+        while(time <= 1)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, COMMON.MATHFUNCTIONS.SquaredSmooth(0, 1, time));
+            time += 0.03f;
             yield return null;
         }
         isCameraLocked = false;
